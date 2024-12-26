24abd3dccb2847ee6fc7e0fabdc80747d73e0192
Thanh Nguyen Dac <thanh4890@gmail.com>
2024-12-26 19:38:21 +0700

    get all categories

--- backend/src/routes/categories/categories.controller.ts
+++ backend/src/routes/categories/categories.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  findAll(): string {
+    return this.categoriesService.findAll();
+  }
+}
--- backend/src/routes/categories/categories.service.ts
+++ backend/src/routes/categories/categories.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  findAll(): string {
+    return `all categories`;
+  }
+}
