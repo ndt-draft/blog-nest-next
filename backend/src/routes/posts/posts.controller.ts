4e40b8a925f15348f6f824da5f8e74a6afa2b560
Thanh Nguyen Dac <thanh4890@gmail.com>
2024-12-29 00:34:08 +0700

    findOne update remove methods

--- backend/src/routes/posts/posts.controller.ts
+++ backend/src/routes/posts/posts.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('posts')
 export class PostsController {
@@ -14,5 +23,20 @@ export class PostsController {
   @Post()
   create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(+id, updatePostDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postsService.remove(+id);
   }
 }
--- backend/src/routes/posts/posts.service.ts
+++ backend/src/routes/posts/posts.service.ts
@@ -1,10 +1,11 @@
 import { Body, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { Post } from './entities/post.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { User } from '../users/entities/user.entity';
 import { CreatePostResponseDto } from './dto/create-post-response.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostsService {
@@ -44,5 +45,36 @@ export class PostsService {
       categories: savedPost.categories,
       user_id: user.id,
     };
+  }
+
+  async findOne(id: number): Promise<Post> {
+    const post = await this.postRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!post) {
+      throw new NotFoundException(`Post with ID ${id} not found`);
+    }
+    return post;
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto): Promise<Post> {
+    await this.findOne(id);
+
+    const updatedPost = await this.postRepository
+      .createQueryBuilder()
+      .update(Post)
+      .set(updatePostDto)
+      .where('id = :id', { id })
+      .returning('*')
+      .execute();
+
+    return updatedPost.raw[0];
+  }
+
+  async remove(id: number): Promise<void> {
+    const post = await this.findOne(id);
+    await this.postRepository.remove(post);
   }
 }
