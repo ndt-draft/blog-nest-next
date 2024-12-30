1c5593ec07a8ad75d42259a20a063e5d903fe267
Thanh Nguyen Dac <thanh4890@gmail.com>
2024-12-31 00:12:41 +0700

    remove because auth guard is used globally

--- backend/src/routes/posts/posts.module.ts
+++ backend/src/routes/posts/posts.module.ts
@@ -4,10 +4,9 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { Post } from './entities/post.entity';
 import { User } from '../users/entities/user.entity';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, User]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Post, User])],
   controllers: [PostsController],
   providers: [PostsService],
 })
