69eff3b00c7ede955a205cd95220fb99c0eac18f
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-01-14 01:32:59 +0700

    support swagger for /categories endpoints

--- backend/src/routes/categories/categories.controller.ts
+++ backend/src/routes/categories/categories.controller.ts
@@ -14,6 +14,7 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 import { PermissionsGuard } from '../auth/permissions/permissions.guard';
 import { CanCreatePost } from '../auth/permissions/permissions.decorator';
 import { Public } from '../auth/jwt/jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('categories')
 export class CategoriesController {
@@ -22,6 +23,7 @@ export class CategoriesController {
   @UseGuards(PermissionsGuard)
   @CanCreatePost()
   @Post()
+  @ApiBearerAuth()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
   }
@@ -41,6 +43,7 @@ export class CategoriesController {
   @UseGuards(PermissionsGuard)
   @CanCreatePost()
   @Patch(':id')
+  @ApiBearerAuth()
   update(
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -51,6 +54,7 @@ export class CategoriesController {
   @UseGuards(PermissionsGuard)
   @CanCreatePost()
   @Delete(':id')
+  @ApiBearerAuth()
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(+id);
   }
