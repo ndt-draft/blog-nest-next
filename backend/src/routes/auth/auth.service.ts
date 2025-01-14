5cb010b7ad63ab9a3990df488b3cc8bfdc917cc0
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-01-15 00:17:10 +0700

    hashing password with bcrypt

--- backend/src/routes/auth/auth.service.ts
+++ backend/src/routes/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +12,10 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
-    if (user && user.password === password) {
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (user && isMatch) {
       const { password, ...result } = user;
       return result;
     }
