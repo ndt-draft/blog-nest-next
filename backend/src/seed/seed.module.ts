079fdcb72310cb1a2d961ac5d0647abe6820a4f3
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-01-18 01:05:02 +0700

    use same db config with app module

--- backend/src/seed/seed.module.ts
+++ backend/src/seed/seed.module.ts
@@ -4,19 +4,11 @@ import { User } from '../routes/users/entities/user.entity';
 import { Post } from '../routes/posts/entities/post.entity';
 import { Category } from '../routes/categories/entities/category.entity';
 import { SeedService } from './seed.service'; // Import your SeedService
+import { DatabaseModule } from '../database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres', // Adjust for your DB type
-      host: 'localhost',
-      port: 5432,
-      username: 'thanhnguyen',
-      password: '',
-      database: 'testdb',
-      entities: [User, Post, Category], // Your entities
-      synchronize: true,
-    }),
+    DatabaseModule,
     TypeOrmModule.forFeature([User, Post, Category]), // Register repositories
   ],
   providers: [SeedService], // Register the SeedService
