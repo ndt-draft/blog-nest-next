8a733d9f3d98ab0daa42cccf44674f3f9f898977
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-01-18 01:03:59 +0700

    avoid drop column instead of no going back

--- backend/src/migrations/1735319766081-AlterTitleTypePostsTable.ts
+++ backend/src/migrations/1735319766081-AlterTitleTypePostsTable.ts
@@ -1,16 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AlterTitleTypePostsTable1735319766081 implements MigrationInterface {
-    name = 'AlterTitleTypePostsTable1735319766081'
+export class AlterTitleTypePostsTable1735319766081
+  implements MigrationInterface
+{
+  name = 'AlterTitleTypePostsTable1735319766081';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "posts" DROP COLUMN "title"`);
-        await queryRunner.query(`ALTER TABLE "posts" ADD "title" character varying(255) NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "posts" ALTER COLUMN "title" TYPE character varying(255)`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "posts" DROP COLUMN "title"`);
-        await queryRunner.query(`ALTER TABLE "posts" ADD "title" character varying NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "posts" ALTER COLUMN "title" TYPE character varying`,
+    );
+  }
 }
--- backend/src/migrations/1735320919787-AlterNameTypeUsersTable.ts
+++ backend/src/migrations/1735320919787-AlterNameTypeUsersTable.ts
@@ -1,16 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AlterNameTypeUsersTable1735320919787 implements MigrationInterface {
-    name = 'AlterNameTypeUsersTable1735320919787'
+export class AlterNameTypeUsersTable1735320919787
+  implements MigrationInterface
+{
+  name = 'AlterNameTypeUsersTable1735320919787';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "name"`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "name" character varying(255) NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "name" TYPE character varying(255)`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "name"`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "name" character varying NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "name" TYPE character varying`,
+    );
+  }
 }
