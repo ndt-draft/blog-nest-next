7e0d846374b4f0995b61f71ec3e8796fc827b503
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-01-21 00:00:55 +0700

    add e2e test for patch /posts/:id

--- backend/test/posts.e2e-spec.ts
+++ backend/test/posts.e2e-spec.ts
@@ -9,7 +9,9 @@ describe('PostsController (e2e)', () => {
   let app: INestApplication;
   let dataSource: DataSource;
   let seedService: SeedService;
-  let jwtToken: string;
+  let jwtAdminToken: string;
+  let jwtAuthorToken: string;
+  let jwtUserToken: string;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -28,12 +30,22 @@ describe('PostsController (e2e)', () => {
     await seedService.seed();
 
     // Obtain JWT token, you may replace this part with your own login endpoint
-    const loginResponse = await request(app.getHttpServer())
+    const loginAdminResponse = await request(app.getHttpServer())
       .post('/auth/login') // Assuming your login endpoint is /auth/login
       .send({ email: 'admin@example.com', password: '1234' })
       .expect(201);
+    const loginAuthorResponse = await request(app.getHttpServer())
+      .post('/auth/login') // Assuming your login endpoint is /auth/login
+      .send({ email: 'author@example.com', password: '1234' })
+      .expect(201);
+    const loginUserResponse = await request(app.getHttpServer())
+      .post('/auth/login') // Assuming your login endpoint is /auth/login
+      .send({ email: 'user@example.com', password: '1234' })
+      .expect(201);
 
-    jwtToken = loginResponse.body.access_token; // Assuming access_token is returned in the body
+    jwtAdminToken = loginAdminResponse.body.access_token; // Assuming access_token is returned in the body
+    jwtAuthorToken = loginAuthorResponse.body.access_token; // Assuming access_token is returned in the body
+    jwtUserToken = loginUserResponse.body.access_token; // Assuming access_token is returned in the body
   });
 
   beforeEach(async () => {
@@ -48,25 +60,64 @@ describe('PostsController (e2e)', () => {
     await app.close();
   });
 
-  it('/posts (GET)', async () => {
-    const response = await request(app.getHttpServer())
-      .get('/posts')
-      .expect(200);
+  describe('posts', () => {
+    it('/posts (GET)', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/posts')
+        .expect(200);
 
-    expect(response.body.posts).toHaveLength(2);
-    expect(response.body.posts[0]).toHaveProperty(
-      'title',
-      'Understanding TypeORM Relationships',
-    );
+      expect(response.body.posts).toHaveLength(2);
+      expect(response.body.posts[0]).toHaveProperty(
+        'title',
+        'Understanding TypeORM Relationships',
+      );
+    });
+
+    describe('/posts (PATCH)', () => {
+      it('should patch /posts/:id by admin', async () => {
+        const response = await request(app.getHttpServer())
+          .patch('/posts/1')
+          .set('Authorization', `Bearer ${jwtAdminToken}`)
+          .send({ title: 'edit by admin' })
+          .expect(200);
+
+        expect(response.body).toHaveProperty('id', 1);
+        expect(response.body).toHaveProperty('title', 'edit by admin');
+      });
+      it('should patch /posts/:id by author', async () => {
+        const response = await request(app.getHttpServer())
+          .patch('/posts/1')
+          .set('Authorization', `Bearer ${jwtAuthorToken}`)
+          .send({ title: 'edit by author' })
+          .expect(200);
+
+        expect(response.body).toHaveProperty('id', 1);
+        expect(response.body).toHaveProperty('title', 'edit by author');
+      });
+      it('should not patch /posts/:id by user', async () => {
+        const response = await request(app.getHttpServer())
+          .patch('/posts/1')
+          .set('Authorization', `Bearer ${jwtUserToken}`)
+          .send({ title: 'edit by user' })
+          .expect(403);
+
+        expect(response.body).toHaveProperty(
+          'message',
+          'You do not have permission to access this resource',
+        );
+      });
+    });
   });
 
-  it('/users (GET)', async () => {
-    const response = await request(app.getHttpServer())
-      .get('/users')
-      .set('Authorization', `Bearer ${jwtToken}`) // Add JWT token here
-      .expect(200);
+  describe('users', () => {
+    it('/users (GET)', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/users')
+        .set('Authorization', `Bearer ${jwtAdminToken}`) // Add JWT token here
+        .expect(200);
 
-    expect(response.body).toHaveLength(3);
-    expect(response.body[0]).toHaveProperty('email', 'admin@example.com');
+      expect(response.body).toHaveLength(3);
+      expect(response.body[0]).toHaveProperty('email', 'admin@example.com');
+    });
   });
 });
