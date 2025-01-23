6996ae1fecf876387c431ae748549552c5fbdfe5
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-01-24 00:36:42 +0700

    ssg for posts, ssr for categories

---
+++ frontend/pages/categories/[id].tsx
@@ -0,0 +1,21 @@
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import { Category } from "@/types/category";
+
+export const getServerSideProps = (async ({ params }) => {
+  // Fetch data from external API
+  const res = await fetch(`http://localhost:3333/categories/${params?.id}`);
+  const category: Category = await res.json();
+  // Pass data to the page via props
+  return { props: { category } };
+}) satisfies GetServerSideProps<{ category: Category }, { id: string }>;
+
+export default function Page({
+  category,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <main>
+      <h2>{category.id}</h2>
+      <p>{category.name}</p>
+    </main>
+  );
+}
--- frontend/pages/posts/[id].tsx
+++ frontend/pages/posts/[id].tsx
@@ -4,28 +4,36 @@ import type {
   GetStaticPaths,
 } from "next";
 import { Post } from "@/types/post";
+import { useRouter } from "next/router";
 
 export const getStaticPaths = (async () => {
   // Call an external API endpoint to get posts
-  const res = await fetch("http://localhost:3333/posts");
-  const data = await res.json();
+  // const res = await fetch("http://localhost:3333/posts");
+  // const data = await res.json();
 
   // Get the paths we want to prerender based on posts
   // In production environments, prerender all pages
   // (slower builds, but faster initial page load)
-  const paths = data.posts.map((post: Post) => ({
-    params: { id: `${post.id}` },
-  }));
+  const paths = [
+    {
+      params: { id: `1` },
+    },
+    {
+      params: { id: `2` },
+    },
+  ];
 
   // { fallback: false } means other routes should 404
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 }) satisfies GetStaticPaths;
 
-export const getStaticProps = (async (context) => {
-  const { params } = context;
-
+export const getStaticProps = (async ({ params }) => {
   const res = await fetch(`http://localhost:3333/posts/${params?.id}`);
   const post = await res.json();
+
+  if (!post) {
+    return { notFound: true };
+  }
 
   return { props: { post } };
 }) satisfies GetStaticProps<{
@@ -35,6 +43,12 @@ export const getStaticProps = (async (context) => {
 export default function Page({
   post,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h2>{post.title}</h2>
