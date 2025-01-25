3e145cb2306c47fa938fca8c9f22ef28439d8e7a
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-01-25 12:14:10 +0700

    add notFound for posts and categories

--- frontend/pages/categories/[id].tsx
+++ frontend/pages/categories/[id].tsx
@@ -4,6 +4,11 @@ import { Category } from "@/types/category";
 export const getServerSideProps = (async ({ params }) => {
   // Fetch data from external API
   const res = await fetch(`http://localhost:3333/categories/${params?.id}`);
+
+  if (!res.ok) {
+    return { notFound: true };
+  }
+
   const category: Category = await res.json();
   // Pass data to the page via props
   return { props: { category } };
--- frontend/pages/posts/[id].tsx
+++ frontend/pages/posts/[id].tsx
@@ -8,20 +8,15 @@ import { useRouter } from "next/router";
 
 export const getStaticPaths = (async () => {
   // Call an external API endpoint to get posts
-  // const res = await fetch("http://localhost:3333/posts");
-  // const data = await res.json();
+  const res = await fetch("http://localhost:3333/posts");
+  const data = await res.json();
 
   // Get the paths we want to prerender based on posts
   // In production environments, prerender all pages
   // (slower builds, but faster initial page load)
-  const paths = [
-    {
-      params: { id: `1` },
-    },
-    {
-      params: { id: `2` },
-    },
-  ];
+  const paths = data.posts.map((post: Post) => ({
+    params: { id: `${post.id}` },
+  }));
 
   // { fallback: false } means other routes should 404
   return { paths, fallback: true };
@@ -29,12 +24,12 @@ export const getStaticPaths = (async () => {
 
 export const getStaticProps = (async ({ params }) => {
   const res = await fetch(`http://localhost:3333/posts/${params?.id}`);
-  const post = await res.json();
 
-  if (!post) {
+  if (!res.ok) {
     return { notFound: true };
   }
 
+  const post = await res.json();
   return { props: { post } };
 }) satisfies GetStaticProps<{
   post: Post;
