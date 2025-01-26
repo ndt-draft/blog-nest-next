2095e127bb9a41fb1e33cf0fd0fcb37305417457
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-01-27 00:18:06 +0700

    show posts by category

--- frontend/pages/categories/[id].tsx
+++ frontend/pages/categories/[id].tsx
@@ -1,26 +1,42 @@
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import { Category } from "@/types/category";
+import { Post } from "@/types/post";
 
 export const getServerSideProps = (async ({ params }) => {
   // Fetch data from external API
-  const res = await fetch(`http://localhost:3333/categories/${params?.id}`);
+  const res = await Promise.all([
+    fetch(`http://localhost:3333/categories/${params?.id}`),
+    fetch(`http://localhost:3333/posts?category=${params?.id}`),
+  ]);
 
-  if (!res.ok) {
+  if (!res[0].ok) {
     return { notFound: true };
   }
 
-  const category: Category = await res.json();
+  const category: Category = await res[0].json();
+  const { posts } = await res[1].json();
+
   // Pass data to the page via props
-  return { props: { category } };
-}) satisfies GetServerSideProps<{ category: Category }, { id: string }>;
+  return { props: { category, posts } };
+}) satisfies GetServerSideProps<
+  { category: Category; posts: Post[] },
+  { id: string }
+>;
 
 export default function Page({
   category,
+  posts,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <main>
       <h2>{category.id}</h2>
       <p>{category.name}</p>
+
+      <ol>
+        {posts.map((post: Post) => (
+          <li>{post.title}</li>
+        ))}
+      </ol>
     </main>
   );
 }
