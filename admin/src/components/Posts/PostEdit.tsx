20bdbce634f975e7bce055e60abe85fc7ec36b22
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-03-12 00:56:34 +0700

    remove unused code

--- admin/src/components/Posts/PostEdit.tsx
+++ admin/src/components/Posts/PostEdit.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { getPost, updatePost } from "@/api/posts";
 import PostForm from "./PostForm";
