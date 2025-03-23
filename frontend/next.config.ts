ccaf4be5759d967dbd7cd200a6f3f0e04aaeed83
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-03-23 16:34:43 +0700

    deploy frontend that supports ssg, isr, ssg through s3 and lambda

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "standalone", // Required for AWS Lambda
+  images: {
+    unoptimized: true, // Disable Next.js image optimization (use S3 instead)
+  },
+  assetPrefix: process.env.NEXT_PUBLIC_ASSETS_PREFIX || "", // Use for S3 asset hosting
 };
 
 export default nextConfig;
