04edb64395c98a510e9fcef1e713bb9ef994e193
Thanh Nguyen Dac <thanh4890@gmail.com>
2025-03-29 16:02:44 +0700

    correct next-js serverless plugin

--- frontend/frontend.js
+++
@@ -1,9 +0,0 @@
-const server = require("./server.js");
-const { createServer, proxy } = require("aws-serverless-express");
-
-const expressApp = server.app;
-const serverlessApp = createServer(expressApp);
-
-module.exports.handler = (event, context) => {
-  return proxy(serverlessApp, event, context);
-};
